cc34e3a5d38bcac6c60a1321854b2677351d4a8b
Arjun_Krishna_Babu <arjunkbabu@users.noreply.github.com>
2014-08-23 16:34:54 +0530

    Fifth Commit

--- linklistclass.cpp
+++ linklistclass.cpp
@@ -15,6 +15,7 @@
 	420H14 -- Arjun Krishna Babu
 */
 #include<iostream>
+
 using namespace std;
 
 class Node	{
@@ -22,144 +23,31 @@ private:
 	int data;
 	Node *next;
 public:
-	Node()	{
-		next = NULL;
-	}
+	Node(): next(NULL)	{ }			//constructor to initialize next address to NULL
+
 	/* setter and getter for data */
-	void setData(int d)	{
-		data = d;
-	}
-
-	int getData()	{
-		return data;
-	}
-
+	void setData(int);
+	int getData();
+	
 	/*	setter and getter for link */
-	void setNext(Node *A)	{
-		next = A;
-	}
-	Node * getNext()	{
-		return next;
-	}
+	void setNext(Node *A);
+	Node* getNext();
 };
 
 class LinkList	{
 private:
 	Node *head;		//keeps track of beginning of node
 public:
-	LinkList()	{
-		head = NULL;	//very imp. Ensures list is initially empty
-	}
-	void insBeg(int dd)	{
-		Node *temp = new Node();	//creates a new node
-		temp->setData(dd);			//sets value to the new node
-
-		if(head == NULL)	{	//if the link-list is empty
-//			temp->setNext(NULL);
-			head = temp;
-		}
-		else	{
-			temp->setNext(head);	//make the newly-created node point to the existing head
-			head = temp;
-		}
-	}
-
-	void insEnd(int dd)	{
-		Node *temp = new Node();
-		temp->setData(dd);
-		temp->setNext(NULL);
-		
-		Node *loc;
-
-		if(head == NULL)	{
-			head = temp;
-		}
-
-		else	{
-			loc = head;
-
-			while(loc->getNext() != NULL)	{	//loop to traverse to the last node in the list
-				loc = loc->getNext();
-			}
-		
-			loc->setNext(temp); 		//make loc(last node) point to the newly created node
-		}
-	}
-
-	void delBeg()	{
-		Node *temp;
-		
-		if(head == NULL)	{
-			cout<<" ERROR: Cannot delete empty linked list";
-		}
-		else	{
-			temp = head;
-
-			cout<<"\n Deleting element: "<<temp->getData()<<"\n\n";
-			head = head->getNext();	//make head point to the next element in the linked list
-			delete temp;
-		}
-	}
-
-	void delEnd()	{
-
-		Node *temp;
-		Node *prev;
-
-		if(head == NULL)	{
-			cout<<"\n ERROR: Cannot delete empty linked list";
-		}
-
-		else if(head->getNext() == NULL)	{	//if only one element exists in the linked list
-			temp = head;
-			head = NULL;
-			delete temp;		//free the allocated memory
-		}
-		else	{
-			temp = head->getNext();
-			prev = head;
-
-			while( temp->getNext() != NULL)	{ //traverse to the end of the linked list
-				temp = temp->getNext();
-				prev = prev->getNext();
-			}
-
-			prev->setNext(temp->getNext());
-			
-			cout<<"\n Deleting element: "<<temp->getData();
-			delete temp;		//free the allocated memory
-		}
-	}
-
-	bool search(int item)	{
-		Node *temp = head;
-
-		while(temp != NULL)	{
-			if(temp->getData() == item)	{
-				return true;
-			}
-			else
-				temp = temp->getNext();
-		}
-		
-		return false;		//control reaches here only after finishing while loop (==> element absent in list)
-	}
-
-	void traverse()	{
-		Node *loc = head;
-		if(head == NULL)	{
-			cout<<" ERROR: EMPTY LIST!\n";
-			return;
-		}
-
-		while(loc != NULL)	{			
-			cout<<"-->"<<(loc->getData());
-			loc = loc->getNext();
-		}
-
-		cout<<"\n\n";
-	}
-
+	LinkList(): head(NULL) { }	//constructor to intialize current address to NULL
+	
+	void insBeg(int);
+	void insEnd(int);
+	
+	void delBeg();
+	void delEnd();
+	
+	bool search(int);
+	void traverse();	
 };
 
 int main()	{
@@ -183,7 +71,7 @@ int main()	{
 					cin>>num;
 					L.insBeg(num);
 					break;
-			case 2: cout<<"\n Enter an element to insert into beginning of the linked list: ";
+			case 2: cout<<"\n Enter an element to insert into end of the linked list: ";
 					cin>>num;
 					L.insEnd(num);
 					break;
@@ -200,7 +88,7 @@ int main()	{
 					break;
 			case 9: cout<<"\n Elements in the linked list: "; L.traverse();
 					break;
-			case 0: cout<<"\n Program under development by Arjun K. Babu";
+			case 0: cout<<"\n Program under development by Arjun Krishna Babu";
 					cout<<"\n https://github.com/arjunkbabu/linked_list";
 					break;
 		   default: cout<<"\n ERROR: Invalid Choice!";
@@ -212,4 +100,127 @@ int main()	{
 
    cout<<"\n\n";
    return 0;
+}
+/*** MEMBER FUNCTION DEFINITIONS ***/
+
+/*** NODE CLASS FUNCTION DEFINITIONS: */
+void Node::setData(int d)	{
+	data = d;
+}
+
+int Node::getData()	{
+	return data;
+}
+
+void Node::setNext(Node *A)	{
+	next = A;
+}
+
+Node* Node::getNext()	{
+	return next;
+}
+/*	*	*	*	*	*	*	*	*	*/
+
+/*** LINKLIST CLASS FUNCTION DEFINITONS: */
+void LinkList::insBeg(int dd)	{
+	Node *temp = new Node();	//creates a new node
+	temp->setData(dd);			//sets value to the new node
+
+	if(head == NULL)	{	//if the link-list is empty
+//		temp->setNext(NULL);
+		head = temp;
+	}
+	else	{
+		temp->setNext(head);	//make the newly-created node point to the existing head
+		head = temp;
+	}
+}
+
+void LinkList::insEnd(int dd)	{
+	Node *temp = new Node();
+	temp->setData(dd);
+	temp->setNext(NULL);
+
+	Node *loc;
+
+	if(head == NULL)	{
+		head = temp;
+	}
+	else	{
+		loc = head;
+		while(loc->getNext() != NULL)	{	//loop to traverse to the last node in the list
+			loc = loc->getNext();
+		}
+	
+		loc->setNext(temp); 		//make loc(last node) point to the newly created node
+	}
+}
+
+void LinkList::delBeg()	{
+	Node *temp;
+	
+	if(head == NULL)	{
+		cout<<" ERROR: Cannot delete empty linked list";
+	}
+	else	{
+		temp = head;
+		cout<<"\n Deleting element: "<<temp->getData()<<"\n\n";
+		head = head->getNext();	//make head point to the next element in the linked list
+		delete temp;
+	}
+}
+
+void LinkList::delEnd()	{
+	Node *temp;
+	Node *prev;
+	if(head == NULL)	{
+		cout<<"\n ERROR: Cannot delete empty linked list";
+	}
+
+	else if(head->getNext() == NULL)	{	//if only one element exists in the linked list
+		temp = head;
+		head = NULL;
+		delete temp;		//free the allocated memory
+	}
+	else	{
+		temp = head->getNext();
+		prev = head;
+
+		while( temp->getNext() != NULL)	{ //traverse to the end of the linked list
+			temp = temp->getNext();
+			prev = prev->getNext();
+		}
+		prev->setNext(temp->getNext());
+			
+		cout<<"\n Deleting element: "<<temp->getData();
+		delete temp;		//free the allocated memory
+	}
+}
+
+bool LinkList::search(int item)	{
+	Node *temp = head;
+
+	while(temp != NULL)	{
+		if(temp->getData() == item)	{
+			return true;
+		}
+		else
+			temp = temp->getNext();
+	}
+	
+	return false;		//control reaches here only after finishing while loop (==> element absent in list)
+}
+
+void LinkList::traverse()	{
+	Node *loc = head;
+	if(head == NULL)	{
+		cout<<" ERROR: EMPTY LIST!\n";
+		return;
+	}
+
+	while(loc != NULL)	{			
+		cout<<"-->"<<(loc->getData());
+		loc = loc->getNext();
+	}
+	cout<<"\n\n";
 }
