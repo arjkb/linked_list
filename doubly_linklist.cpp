b031e1ee841d83b2c6c354f2fd2a7c34f96a30e7
Arjun_Krishna_Babu <arjunkbabu@users.noreply.github.com>
2014-09-21 16:44:36 +0530

    Fourteenth Commit

--- doubly_linklist.cpp
+++ doubly_linklist.cpp
@@ -57,6 +57,11 @@ public:
 	
 	void insHead(int);
 	void insTail(int);
+	void insAfter(int elem, int pos);
+	
+	void delHead();
+	void delTail();
+	void delElem(int);
 	
 	bool search(int);
 	
@@ -69,12 +74,18 @@ int main()	{
 	DoubleLinkList DL;
 	
 	int choice;
-	int num;
+	int num, pos;
 	
 	do {
 		cout<<"\n [DOUBLY LINKED LIST IMPLEMENTATION]";
 		cout<<"\n 1. Insert to head";
 		cout<<"\n 2. Insert to tail";
+		cout<<"\n 3. Insert after an element";
+		cout<<"\n\n";
+		cout<<"\n 4. Delete from head";
+		cout<<"\n 5. Delete from tail";
+		cout<<"\n 6. Delete particular element";
+		cout<<"\n\n";
 		cout<<"\n 7. Search";
 		cout<<"\n 8. In-order traversal";
 		cout<<"\n 9. Reverse-order traversal";
@@ -89,6 +100,20 @@ int main()	{
 			case 2: cout<<"\n Enter element to insert to tail: ";
 					cin>>num;
 					DL.insTail(num);
+					break;
+			case 3: cout<<"\n Enter element to insert into list: ";
+					cin>>num;
+					cout<<"\n After which element should it be inserted: ";
+					cin>>pos;
+					DL.insAfter(num, pos);
+					break;
+			case 4: DL.delHead();
+					break;
+			case 5: DL.delTail();
+					break;
+			case 6: cout<<"\n Enter element you wish to delete: ";
+					cin>>num;
+					DL.delElem(num);
 					break;
 			case 7: cout<<"\n Enter element to search: ";
 					cin>>num;
@@ -164,6 +189,125 @@ void DoubleLinkList::insTail(int d)	{
 	temp->setPrev(loc);
 }
 
+void DoubleLinkList::insAfter(int d, int pos)	{
+	Node *temp = new Node(d);
+	Node *loc  = head;
+	
+	if( isEmpty() )	{
+		PRINT_EMPTY_ERROR;
+	}
+	else {
+		while( loc != NULL )	{
+			if( loc->getData() == pos )	{	
+				if( loc->getNext() == NULL )	{
+					insTail(d);
+				}
+				else {	
+					temp->setPrev(loc);					
+					temp->setNext( loc->getNext() ); //set the pointers of temp
+					
+					loc = loc->getNext();			//go to node after temp
+					loc->setPrev(temp);				//set the pointers
+				
+					loc = temp->getPrev();			//go to node before temp
+					loc->setNext(temp);				//set the pointers
+				}
+				
+			  return;
+			}
+			else
+				loc = loc->getNext();
+		}
+	}
+	
+	cerr<<"\n ERROR: "<<pos<<" not found in list!";	//control reaches here only if pos wasn't found
+}
+	
+void DoubleLinkList::delHead()	{
+	Node *temp = head;
+	
+	if( isEmpty() )	{
+		PRINT_EMPTY_ERROR;
+	}
+	
+	else if( head->getNext() == NULL )	{
+		cout<<"\n Deleting Element: "<<temp->getData();		//temps points to head btw
+		head = NULL;
+		delete temp;	
+	}	
+	else {
+		cout<<"\n Deleting Element: "<<temp->getData();
+		
+		head = head->getNext();
+		head->setPrev(NULL);
+		
+		delete temp;
+	}
+}
+
+void DoubleLinkList::delTail()	{
+	Node *temp = head;
+	Node *loc;
+	if( isEmpty() )	{
+		PRINT_EMPTY_ERROR;
+	}
+	else if( head->getNext() == NULL )	{
+		cout<<"\n Deleting Element: "<<temp->getData();		//temps points to head btw
+		head = NULL;
+		delete temp;
+	}
+	else {
+		
+		while( temp->getNext() != NULL )	{	//traverse to the end of the list
+			loc = temp;
+			temp = temp->getNext();
+		}
+		
+		loc->setNext(NULL);
+		
+		cout<<"\n Deleting Element: "<<temp->getData();
+		
+		delete temp;
+	}
+}
+
+void DoubleLinkList::delElem(int d)	{
+	Node *temp = head;
+	Node *loc = head;
+
+	if( isEmpty() )	{
+		PRINT_EMPTY_ERROR;
+	}
+	else {	
+		while( temp != NULL )	{
+			if( temp->getData() == d )	{
+				if( temp == head )	
+					delHead();
+				else if( temp->getNext() == NULL )
+					delTail();
+				else {
+					loc->setNext( temp->getNext() );
+					
+					loc = loc->getNext();
+					
+					loc->setPrev( temp->getPrev() );
+					
+					cout<<"\n Deleting Element: "<<temp->getData();
+					
+				}
+			  return;
+			}
+			else {
+				loc = temp;
+				temp = temp->getNext();
+			}			
+	
+		}	//end of while()
+		
+		cerr<<"\n ERROR: "<<d<<" not found in Linked List!";
+	}
+}
+
 bool DoubleLinkList::search(int key)	{
 	Node *temp = head;
 
@@ -185,7 +329,7 @@ void DoubleLinkList::traverse_inorder()	{
 		return;
 	}
 	
-	cout<<" NULL ";
+	cout<<" HEAD ";
 	while( temp != NULL )	{
 		cout<<" <--> "<<temp->getData();
 		temp = temp->getNext();
@@ -209,5 +353,5 @@ void DoubleLinkList::traverse_revorder()	{
 		cout<<" <--> "<<temp->getData();
 		temp = temp->getPrev();
 	}
-	cout<<" <--> NULL ";
+	cout<<" <--> HEAD ";
 }
